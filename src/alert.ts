db416ea839d6a1457ce6394d9f1ce3d335b3084b
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-10-02 12:35:48 +0400

    chore: add multiple chat ids

--- src/alert.ts
+++ src/alert.ts
@@ -2,22 +2,29 @@ import * as dotenv from "dotenv";
 import fetch from "node-fetch";
 
 dotenv.config();
+
 export const sentTelegramAlert = async (text: string) => {
+  const chatIds = process.env.CHAT_ID?.split(",") ?? [];
+
   try {
-    const res = await fetch(
-      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          chat_id: process.env.CHAT_ID,
-          text: `${text} at ${new Date().toLocaleString()}`,
-        }),
-        headers: {
-          "content-type": "application/json",
-        },
-      }
-    );
+    await Promise.all(chatIds.map((chatId) => sentAlertToUser(chatId, text)));
   } catch (err) {
     console.log("Failed to notify");
   }
+};
+
+export const sentAlertToUser = async (chatId: string, text: string) => {
+  return fetch(
+    `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        chat_id: chatId.trim(),
+        text: `${(text ?? "").toUpperCase()} at ${new Date().toLocaleString()}`,
+      }),
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
 };
